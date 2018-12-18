fc2ff8cba3e11a67f1e9256f0708e032516a8d77
YMilton <278251850@qq.com>
2018-12-18 23:03:55 +0800

    add leave_one_out method

--- chapter3/3_4.py
+++ chapter3/3_4.py
@@ -68,14 +68,19 @@ def gradDescent(X, y, lr=0.05, iters=150):
     return beta
 
 
-def ten_fold_cross_once(data):
+def ten_fold_cross_once(data, choice_labels=[1,2]):
+    '''
+    10次交叉验证，一次
+    :param data:
+    :return: 返回预测的准确率
+    '''
     # 随机生成的数组，随机的长度，随机是否重复
-    choice_labes = np.random.choice([1,2,3],2,False)
-    print('random choice labels: ', choice_labes)
+    #choice_labels = np.random.choice([1,2,3],2,False)
+    print('random choice labels: ', choice_labels)
     # 选择随机标签的数据, 包括标签
-    choice_data0 = data.values[data.values[:,-1] == choice_labes[0],:]
-    choice_data1 = data.values[data.values[:,-1] == choice_labes[1],:]
-    # 随机生成角标
+    choice_data0 = data.values[data.values[:,-1] == choice_labels[0],:]
+    choice_data1 = data.values[data.values[:,-1] == choice_labels[1],:]
+    # 随机生成角标, 45生成的size, false不重复
     rand_indexs = np.random.choice(50,45,False)
     # 剩下未被筛选的角标
     other_incexs = [x for x in range(50) if x not in rand_indexs]
@@ -87,8 +92,9 @@ def ten_fold_cross_once(data):
     train_y = np.vstack((choice_data0[rand_indexs,-1:], choice_data1[rand_indexs,-1:]))
     test_y = np.vstack((choice_data0[other_incexs, -1:], choice_data1[other_incexs, -1:]))
     # set the label to [0,1]
-    train_y = np.array([0 if x== train_y[0] else 1 for x in train_y]).reshape(-1,1)
-    test_y = np.array([0 if x == test_y[0] else 1 for x in test_y]).reshape(-1,1)
+    positive_label = train_y[0]
+    train_y = np.array([1 if x== positive_label else 0 for x in train_y]).reshape(-1,1)
+    test_y = np.array([1 if x == positive_label else 0 for x in test_y]).reshape(-1,1)
     # logistic function
     beta = gradDescent(train_data, train_y, iters=150)
 
@@ -104,13 +110,69 @@ def ten_fold_cross_once(data):
     return accuracy_rate
 
 def ten_fold_cross_many():
+    '''
+    随机选择几次得到准确率，算平均值
+    :return:
+    '''
     acc_rates = []
-    for i in range(10):
-        rate = ten_fold_cross_once(label2digit())
+    choice_labels = [[1,2],[1,3],[2,3]]
+    for i in range(3):
+        rate = ten_fold_cross_once(label2digit(), choice_labels[i])
+        acc_rates.append(rate)
+    print(acc_rates)
+    print(np.mean(acc_rates))
+
+
+def leave_one_out(data):
+    # 随机生成的数组，随机的长度，随机是否重复
+    choice_labes = np.random.choice([1, 2, 3], 2, False)
+    print('random choice labels: ', choice_labes)
+    # 选择随机标签的数据, 包括标签
+    choice_data0 = data.values[data.values[:, -1] == choice_labes[0], :]
+    choice_data1 = data.values[data.values[:, -1] == choice_labes[1], :]
+    # 上下合并数据
+    choice_data = np.vstack((choice_data0, choice_data1))
+    # 随机生成角标
+    rand_indexs = np.random.choice(100, 99, False)
+    # 剩下未被筛选的角标
+    other_incexs = [x for x in range(100) if x not in rand_indexs]
+
+    # 测试集与训练集
+    train_data = choice_data[rand_indexs, :-1]
+    test_data = choice_data[other_incexs, :-1]
+
+    train_y = choice_data[rand_indexs, -1:]
+    test_y = choice_data[other_incexs, -1:]
+    # set the label to [0,1]
+    positive_label = train_y[0]
+    train_y = np.array([1 if x == positive_label else 0 for x in train_y]).reshape(-1, 1)
+    test_y = np.array([1 if x == positive_label else 0 for x in test_y]).reshape(-1, 1)
+    # logistic function
+    beta = gradDescent(train_data, train_y, iters=150)
+
+    # 测试集的计算
+    test_data = np.hstack((test_data, np.ones((test_data.shape[0], 1))))  # 添加b项
+    z = test_data.dot(beta.T)
+    y = sigmoid(z)
+    # 对率回归预测结果
+    predict_y = np.around(y)
+    # print(predict_y==test_y)
+    accuracy_rate = np.sum(predict_y == test_y) / test_y.shape[0]
+
+    return accuracy_rate
+
+
+def leave_one_out_iterion():
+    acc_rates = []
+    for i in range(3):
+        rate = leave_one_out(label2digit())
         acc_rates.append(rate)
     print(acc_rates)
     print(np.mean(acc_rates))
 
 
 if __name__ == '__main__':
+    print('10次交叉验证法：')
     ten_fold_cross_many()
+    print('留一法：')
+    leave_one_out_iterion()
