04d362528ca8156f40bafa22f51f10e6286e96f3
YMilton <278251850@qq.com>
2018-12-18 18:19:22 +0800

    modified 3_3.py

--- chapter3/3_3.py
+++ chapter3/3_3.py
@@ -2,7 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+'''
+3.3 编程实现对率回归，并给出西瓜数据集3.0a上的结果
+'''
 def sigmoid(x):
+    '''
+    sigmoid function definition
+    :param x:
+    :return:
+    '''
     y = 1.0 / (1 + np.exp(-x))
     return y
 
@@ -21,7 +29,7 @@ def newton(X, y):
     old_l = 0.0
     new_l = np.sum(y*z + np.log(1 + np.exp(z))) # loss function
     iters = 0
-    while( np.abs(old_l - new_l) > 1e-5):
+    while( np.abs(old_l - new_l) > 1e-6):
         # shape[N,1]
         p1 = np.exp(z) / (1 + np.exp(z))
         # shape[N,N]
@@ -50,7 +58,6 @@ def gradDescent(X, y):
     :param y:
     :return:
     '''
-    N = X.shape[0]
     lr = 0.05 # 学习率
     # initialization
     beta = np.ones((1,3)) * 0.1
@@ -74,17 +81,36 @@ def gradDescent(X, y):
     return beta
 
 
-
-
 if __name__ == '__main__':
-    p1 = np.random.randint(0,255,(10,1))
-    p2 = np.random.randint(0,255,(1,10))
+    # read the data  (编号，密度，含糖量，是否好瓜)
+    workbook = pd.read_csv('../data/watermelon_3a.csv', header=None)
+    # insert 3 column with ones
+    workbook.insert(3,'3',1)
+    # the datas
+    X = workbook.values[:,1:-1]
+    # the labels
+    y = workbook.values[:,4].reshape(-1,1)
+    # plot training data
+    positive_data = workbook.values[workbook.values[:,4]==1.0, :]
+    negative_data = workbook.values[workbook.values[:,4]==0.0, :]
+    plt.plot(positive_data[:,1], positive_data[:,2], 'bo')# 正例
+    plt.plot(negative_data[:,1], negative_data[:,2], 'r+')# 负例
 
-    print('p1:', p1)
-    print('p2:', p2)
+    # get optimal params beta with newton method
+    beta = newton(X, y)
+    # 绘制直线的边界点
+    newton_left = -( beta[0,0]*0.1 + beta[0,2] ) / beta[0,1]
+    newton_right = -( beta[0,0]*0.9 + beta[0,2] ) / beta[0,1]
+    plt.plot([0.1, 0.9], [newton_left, newton_right], 'g-')
 
-    print(p1.dot(p2))
+    # get optimal params beta with gradient descent method
+    beta = gradDescent(X, y)
+    # 绘制直线的边界点
+    grad_descent_left = -(beta[0, 0] * 0.1 + beta[0, 2]) / beta[0, 1]
+    grad_descent_right = -(beta[0, 0] * 0.9 + beta[0, 2]) / beta[0, 1]
+    plt.plot([0.1, 0.9], [grad_descent_left, grad_descent_right], 'y-')
 
-
-
-
+    plt.xlabel('density')
+    plt.ylabel('sugar rate')
+    plt.title('LR')
+    plt.show()
