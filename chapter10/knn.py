57f7313e649b07947ac6cdc80daf1368342ba225
YMilton <278251850@qq.com>
2019-02-22 15:38:46 +0800

    add chapter10 knn plot

--- chapter10/knn.py
+++ chapter10/knn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import operator
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def classify(inX, dataSet, labels, k):
@@ -30,13 +31,69 @@ def classify(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
-if __name__ == '__main__':
+def predicts(X, k, dataSet, labels):
+    '''
+    批量分类数据
+    :param X: 待分类的
+    :param k:
+    :param dataSet: KNN数据集
+    :param labels: KNN数据集的标签
+    :return:
+    '''
+    LL = []
+    for line in X:
+        l = classify(line, dataSet, labels, k)
+        LL.append(l)
+    return LL
+
+
+def plot_desicion_boundary(X, y, k):
+    '''
+    分类边界
+    :param X:  KNN学习的数据集
+    :param y: KNN学习的数据集对应标签
+    :param k: KNN的k值
+    :return:
+    '''
+    x_min = X[:, 0].min() - 0.1
+    x_max = X[:, 0].max() + 0.1
+    y_min = X[:, 1].min() - 0.1
+    y_max = X[:, 1].max() + 0.1
+    # 在x, y的最大值与最小值之间做网格化
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.03), np.arange(y_min, y_max, 0.03))
+    # 生成待预测的数据集
+    inXs = np.vstack([xx.ravel(), yy.ravel()]).T.tolist()
+    zz = predicts(inXs, k, X, y)
+    # 等高线z轴数据生成，与x,y轴数据大小一致
+    zz = np.array(zz).reshape(xx.shape)
+    # 绘制分界线图
+    plt.figure(figsize=(10,8))
+    plt.contourf(xx, yy, zz, alpha=0.4)
+    plt.scatter(X[:,0], X[:,1], c=y, s=20, edgecolors='k')
+    plt.title('KNN( k={} )'.format(k))
+    plt.show()
+
+
+def test_classify():
+    '''
+    数据做分类预测（前16做分类训练，最后一个数据做分类预测）
+    :return:
+    '''
     data = pd.read_excel('watermelon3.0a.xlsx', index_col='编号')
-    dataSet = data.values[:-1,:-1]
+    dataSet = data.values[:-1, :-1]
 
-    inX = data.values[-1,:-1]
+    inX = data.values[-1, :-1]
+    labels = data.values[:-1, -1]
+    print(classify(inX, dataSet, labels, 5))
 
-    labels = data.values[:-1,-1]
 
-    print(classify(inX, dataSet, labels, 3))
+
+if __name__ == '__main__':
+    # 等高线绘制
+    data = pd.read_excel('watermelon3.0a.xlsx', index_col='编号')
+    dataSet = data.values[:, :-1]
+    labels = data.values[:, -1]
+    labels = [1 if label=='是' else 0 for label in labels]
+
+    plot_desicion_boundary(dataSet, labels, 3)
 
